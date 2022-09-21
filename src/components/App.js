e2f4ee8c8dc007851ab5d7c49b6b5e9e443db579
Ben Lieberman <60364696+bhlieberman@users.noreply.github.com>
2022-09-21 16:39:33 -0600

    pre-stretch goal work

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Footer from "./Footer.js";
 import SelectedBeast from "./SelectedBeast.js";
 import animals from "./animals.json";
 import React from "react";
+import Form from "react-bootstrap/Form";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -14,15 +15,35 @@ class App extends React.Component {
     this.state = {
       show: false,
       selectedBeast: {},
+      filter: null,
     };
     this.showModal = this.showModal.bind(this);
     this.showModalFalse = this.showModalFalse.bind(this);
+    this.fuzzySearch = this.fuzzySearch.bind(this);
+  }
+  fuzzySearch(input, chars) {
+    const regex = new RegExp(chars, "i");
+    this.setState({ filter: regex });
+    return regex.exec(input);
   }
   render() {
     return (
       <div className="App">
-        <Header />
-        <Main data={animals.animals} setShowModal={this.showModal} />
+        <Header className="d-block mb-4" />
+        <Form
+          onChange={(e) => {
+            e.preventDefault();
+            console.log(this.fuzzySearch(e.target.value, "uni"));
+          }}
+        >
+          <Form.Control type="text" placeholder="Search" />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form>
+        <Main
+          data={animals.animals}
+          fuzzy={this.fuzzySearch}
+          setShowModal={this.showModal}
+        />
         <SelectedBeast
           show={this.state.show}
           selectedBeast={this.state.selectedBeast}
